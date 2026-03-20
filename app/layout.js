ef37da9862cb51bbd997a098abda0dcaee7430d6
thevyansh-sin <sinhadivyansh2302@gmail.com>
2026-03-20 17:46:30 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import './globals.css';
 
 export const metadata = {
@@ -8,6 +9,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script 
+        src="https://www.googletagmanager.com/gtag/js?id=G-EBEXGB44Y4"
+        strategy="afterInteractive"
+      />
+      <Script 
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EBEXGB44Y4');
+          `,
+        }}
+      />
       <body>{children}</body>
     </html>
   );
